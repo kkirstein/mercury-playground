ebf0b82655c94e526cab3a3e0433865e3d87e785
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2015-05-08 17:37:07 +0200

    ! beautify output

--- benchmark.m
+++ benchmark.m
@@ -1,6 +1,6 @@
 % benchmark.m Various benchmarks for the Mercury compiler
 %
-% vim: ft=mercury ts=4 sw=4
+% vim: ft=mercury ts=4 sw=4 et
 
 :- module benchmark.
 
@@ -19,42 +19,48 @@
 
 %-----------------------------------------------------------------------------%
 
-:- pred time_fun(string, func(T1) = T2, T1, T2, int, io, io).
-:- mode time_fun(in, func(in) = out is det, in, out, in, di, uo) is cc_multi.
+:- pred time_fun_scalar(string, func(T1) = T2, T1, T2, int, io, io).
+:- mode time_fun_scalar(in, func(in) = out is det, in, out, in, di, uo)
+    is cc_multi.
 
-time_fun(NAME, FUN, IN, RESULT, REP, IO_IN, IO_OUT) :-
-	benchmark_func(FUN, IN, RESULT, REP, Elapsed),
-	io.format("%s = ", [s(NAME)], IO_IN, IO_1),
-	io.write(RESULT, IO_1, IO_2),
-	io.format("\nElapsed time %dms for %d repetitions (%.3fms/run).\n",
-		[i(Elapsed), i(REP), f(float(Elapsed)/float(REP))], IO_2, IO_OUT).
+time_fun_scalar(NAME, FUN, IN, RESULT, REP, IO_IN, IO_OUT) :-
+    benchmark_func(FUN, IN, RESULT, REP, Elapsed),
+    io.format("%s = ", [s(NAME)], IO_IN, IO_1),
+    io.write(RESULT, IO_1, IO_2),
+    io.format("\nElapsed time %dms for %d repetitions (%.3fms/run).\n",
+        [i(Elapsed), i(REP), f(float(Elapsed)/float(REP))], IO_2, IO_OUT).
 
 %-----------------------------------------------------------------------------%
 
 main(!IO) :-
 
-	% Fibonacci numbers
-	% =================
-	Inp1 = 35,
-	Inp2 = integer.integer(1000),
+    % Fibonacci numbers
+    % =================
+    Inp1 = 35,
+    Inp2 = integer.integer(1000),
 
-	time_fun(string.format("fib(%d) (int)", [i(Inp1)]),
-		fib_i, Inp1, _, 10, !IO),
-	io.nl(!IO),
+    time_fun_scalar(string.format("fib(%d) (int)", [i(Inp1)]),
+        fib_i, Inp1, _, 10, !IO),
+    io.nl(!IO),
 
-	time_fun(string.format("fib(%d) (int, tail-recursive) ", [i(Inp1)]),
-		fib_int_t, Inp1, _, 1000, !IO),
-	io.nl(!IO),
+    time_fun_scalar(string.format("fib(%d) (int, tail-recursive) ", [i(Inp1)]),
+        fib_int_t, Inp1, _, 1000, !IO),
+    io.nl(!IO),
 
-	time_fun(string.format("fib(%s) (bigint, tail-recursive) ", [s(integer.to_string(Inp2))]),
-		fib_bigint_t, Inp2, _, 1000, !IO),
-	io.nl(!IO),
+    time_fun_scalar(string.format("fib(%s) (bigint, tail-recursive) ",
+        [s(integer.to_string(Inp2))]), fib_bigint_t, Inp2, _, 1000, !IO),
+    io.nl(!IO),
 
-	% Perfect numbers
-	% ===============
-	benchmark_det(perfect_numbers, 10000, PN, 10, Elapsed_pn),
-	io.write_string("Perfect Numbers: ", !IO),
-	io.write(PN, !IO),
-	io.format(" (Elapsed time for 10 repetitions: %dms)\n", [i(Elapsed_pn)], !IO),
-	io.nl(!IO).
+    % Perfect numbers
+    % ===============
+    benchmark_det(perfect_numbers, 10000, PN, 10, Elapsed_pn),
+    io.write_string("Perfect Numbers: ", !IO),
+    io.write(PN, !IO),
+    io.nl(!IO),
+    io.format("Elapsed time %dms for %d repetitions (%.3fms/run).\n",
+        [i(Elapsed_pn), i(10), f(float(Elapsed_pn)/float(10))], !IO),
+    io.nl(!IO),
+
+    io.write_string("Press <ENTER> to continue..", !IO),
+    io.read_char(stdin_stream, _, !IO).
 
